63267c8c9621b35f420ff7362b91c89e7f34d842
sriram.seshadri@coda.global-at-908843285931 <>
2021-09-20 13:29:03 +0530

    Added secrets file for discount tire - DTFWP-6739

---
+++ src/main/java/com/bb/selfhosted/SelfhostedApplication.java
@@ -0,0 +1,13 @@
+package com.bb.selfhosted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfhostedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfhostedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bb/selfhosted/SelfhostedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb.selfhosted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfhostedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
